6db29ab4afdedc6c7734f67d7ce3bea0ccf0f2e1
banyanshade-software <braun.daniel@free.fr>
2025-01-05 21:43:14 +0100

    cleanup

--- librs/src/lib.rs
+++ librs/src/lib.rs
@@ -23,6 +23,7 @@ extern "C" {
 
 #[no_mangle]
 fn rs_main() -> !{
+
 	// green led : PA5
 	//let peripherals = stm32g491::Peripherals::take().unwrap();
 	let peripherals = unsafe { stm32g491::Peripherals::steal() };
@@ -33,10 +34,14 @@ Longtemps je me suis couche de bonne heure  \
 Parfois a peine ma bougie eteinte  mes yeux se fermaient si vite \
 que je n avais pas le temps de me dire  \
 je m endors  Et une demi heure apres  la pensee qu il etait temps de chercher \
-le someil m eveillait ";
+le someil m eveillait Je voulais poser le volume que je croyais avoir encore \
+dans les mains et souffler ma lumière  je n avais ps cesse en dormant de faire des \
+reflexions sur ce que je venais de lire mais ces reflexions avaient pris un tour \
+un peu particulier  il me semblait que j etais moi meme ce dont parlait l ouvrage";
 	
 	loop {
   		let mut mi = morse_iterator(str);
+  		
     	loop {
 			let n = unsafe { notifWait() };
 			if n & 0x0000_0001 == 0 { continue; }
@@ -44,25 +49,15 @@ le someil m eveillait ";
 			let k = mi.next();
 			match k {
 				None => break,
+				
 				Some(' ') => gpioa.brr().write(|w|   w.br5().set_bit()),
 				_         => gpioa.bsrr().write(|w| w.bs5().set_bit()),
+				//_ => (),
 			};
-    
-			//iprintln!(itm(), "k ");
-			/*
-			if n & 0x0000_0001 != 0 {
-				gpioa.bsrr().write(|w| w.bs5().set_bit());
-			}
-			if n & 0x0000_0002 != 0  {
-				gpioa.brr().write(|w| w.br5().set_bit());
-			}
-			iprintln!(itm(), "aa");
-	    	//unsafe {  osDelay(1000);  }
-	    	//unsafe {  osDelay(200);  }
-	    	*/
 	    }
 	}
 }
+
 
 fn morse(ch:char) -> &'static str
 {
@@ -104,7 +99,7 @@ fn morse(ch:char) -> &'static str
 	conv[i]
 }
 
-fn to_state(m :char) -> &'static str {
+fn to_onoff(m :char) -> &'static str {
 	let r = match m {
 		' ' => "   ",
 		'-' => "xxx ",
@@ -114,23 +109,19 @@ fn to_state(m :char) -> &'static str {
 	r
 }
 
+
+
 fn morse_iterator(str: &str) -> impl Iterator<Item = char> + use<'_>
 {
 	str.chars()
 	   .flat_map(|k| morse(k).chars())
-	   .flat_map(|m| to_state(m).chars())	   
+	   .flat_map(|m| to_onoff(m).chars())	
+	
 }
 
-/*
-fn test_panic() -> Option<()> {
-	None
-}*/
+// stack 844 B
+// 364B with opt
 
-/*
-fn itm() -> &'static mut peripheral::itm::Stim {
-    unsafe { &mut (*peripheral::ITM::PTR).stim[0] }
-}
-*/
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
