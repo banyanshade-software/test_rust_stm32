6eaba5919c384cd6dc1891ce61d62ed9390cb795
banyanshade-software <braun.daniel@free.fr>
2025-02-09 18:34:43 +0100

    unit test added

--- stm32_crates/itm_debug/src/itm.rs
+++ stm32_crates/itm_debug/src/itm.rs
@@ -11,12 +11,11 @@
 //! we also provide several msg domains, because we don't want to
 //! activate all messages at the same time
 
-//use core::{ptr, slice};
 
+#[cfg(not(test))]
 use cortex_m::peripheral::itm::Stim;
+#[cfg(not(test))]
 use cortex_m::peripheral::ITM;
-
-//use cortex_m::iprintln;
 
 
 static MASK :u16 = 0;
@@ -58,6 +57,36 @@ impl Iterator for IntToCharIter {
 }
 
 
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+   
+    #[test]
+    fn test_it1() {
+        let mut it = IntToCharIter::new(1234);
+        assert_eq!(it.next(), Some('1' as u8));
+        assert_eq!(it.next(), Some('2' as u8));
+        assert_eq!(it.next(), Some('3' as u8));
+        assert_eq!(it.next(), Some('4' as u8));
+        assert_eq!(it.next(), None);
+    }
+    #[test]
+    fn test_itloop() {
+        let mut it = IntToCharIter::new(4287234);
+        let mut v: Vec<u8> = Vec::new();
+        for x in it {
+            v.push(x);
+        }
+        let r = ['4' as u8,'2' as u8,'8' as u8,'7' as u8,'2' as u8,'3' as u8,'4' as u8];
+        let rv = r.to_vec();
+        assert_eq!(v, rv);
+    }
+}
+
+
+
+#[cfg(not(test))]
 fn write_all(port: &mut Stim, buffer: &[u8]) {
     for c in buffer {
         while !port.is_fifo_ready() {}
@@ -65,6 +94,7 @@ fn write_all(port: &mut Stim, buffer: &[u8]) {
     }       
 }
 
+#[cfg(not(test))]
 fn write_int(port: &mut Stim, v: i32)
 {
     let it = IntToCharIter::new(v);
@@ -74,6 +104,7 @@ fn write_int(port: &mut Stim, v: i32)
     }
 }
 
+#[cfg(not(test))]
 fn prt3(msg: &str, a: i32, b: i32, c: i32, n: u8) {
     let itm = unsafe { &mut *ITM::PTR };
     let port0 = &mut itm.stim[0];
@@ -126,6 +157,7 @@ macro_rules! itm_debug0 {
 
 
 
+#[cfg(not(test))]
 pub fn t3() {
     itm_debug3!(1, "test", 42, 44, 55);
 }
